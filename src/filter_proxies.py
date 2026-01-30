b3176a37c2ba39294a7fca527f21f5f99009a182
José Alberto <jose.tizon.9009@ga.ita.br>
2026-01-30 02:10:25 -0300

    fix an identation

--- src/filter_proxies.py
+++ src/filter_proxies.py
@@ -1,24 +1,24 @@
-    import pandas as pd
-    import os
+import pandas as pd
+import os
 
-    def filter_by_success_rate(df, folder_path="../proxies"):
-        def categorize(rate):
-            if rate == 0:
-                return "NOT WORKING"
-            elif rate < 0.7:
-                return "BAD"
-            elif rate < 0.9:
-                return "GOOD"
-            else:
-                return "VERY_GOOD"
-        
-        df["status"] = df["success_rate"].apply(categorize)
+def filter_by_success_rate(df, folder_path="../proxies"):
+    def categorize(rate):
+        if rate == 0:
+            return "NOT WORKING"
+        elif rate < 0.7:
+            return "BAD"
+        elif rate < 0.9:
+            return "GOOD"
+        else:
+            return "VERY_GOOD"
+    
+    df["status"] = df["success_rate"].apply(categorize)
 
-        os.makedirs(folder_path, exist_ok=True)
-        
-        for category in ["BAD", "GOOD", "VERY_GOOD"]:
-            df_cat = df[df["status"] == category]
-            if not df_cat.empty:
-                df_cat.to_csv(f"{folder_path}/{category.lower()}_proxies.csv", index=False)
-        
-        return df
+    os.makedirs(folder_path, exist_ok=True)
+    
+    for category in ["BAD", "GOOD", "VERY_GOOD"]:
+        df_cat = df[df["status"] == category]
+        if not df_cat.empty:
+            df_cat.to_csv(f"{folder_path}/{category.lower()}_proxies.csv", index=False)
+    
+    return df
