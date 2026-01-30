6f01c2cfb15d0a95ff20902390346c409d93f041
José Alberto <jose.tizon.9009@ga.ita.br>
2026-01-30 01:23:22 -0300

    change folder of proxy lists

--- src/filter_proxies.py
+++ src/filter_proxies.py
@@ -1,21 +1,24 @@
-import pandas as pd
+    import pandas as pd
+    import os
 
-def filter_by_success_rate(df, folder_path=".."):
-    def categorize(rate):
-        if rate == 0:
-            return "NOT WORKING"
-        elif rate < 0.7:
-            return "BAD"
-        elif rate < 0.9:
-            return "GOOD"
-        else:
-            return "VERY GOOD"
-    
-    df["status"] = df["success_rate"].apply(categorize)
-    
-    for category in ["BAD", "GOOD", "VERY_GOOD"]:
-        df_cat = df[df["status"] == category]
-        if not df_cat.empty:
-            df_cat.to_csv(f"{folder_path}/{category.lower()}_proxies.csv", index=False)
-    
-    return df
+    def filter_by_success_rate(df, folder_path="../proxies"):
+        def categorize(rate):
+            if rate == 0:
+                return "NOT WORKING"
+            elif rate < 0.7:
+                return "BAD"
+            elif rate < 0.9:
+                return "GOOD"
+            else:
+                return "VERY_GOOD"
+        
+        df["status"] = df["success_rate"].apply(categorize)
+
+        os.makedirs(folder_path, exist_ok=True)
+        
+        for category in ["BAD", "GOOD", "VERY_GOOD"]:
+            df_cat = df[df["status"] == category]
+            if not df_cat.empty:
+                df_cat.to_csv(f"{folder_path}/{category.lower()}_proxies.csv", index=False)
+        
+        return df
